df7db89596b1885e6c88726eb426550bc48c50ae
MrIceHavoc <afonso.fernandes100@gmail.com>
2025-11-06 18:21:06 +0000

    Test cpp script now cleans build directory after testing is complete

--- scripts/test_cpp.sh
+++ scripts/test_cpp.sh
@@ -2,3 +2,16 @@ cmake -B backend/build
 cmake --build backend/build
 cd backend/build
 ctest
+rm -rf _deps
+rm -rf bin
+rm -rf CMakeFiles
+rm -rf lib
+rm -rf Testing
+rm -rf cmake_install.cmake
+rm -rf CMakeCache.txt
+rm -rf CTestTestfile.cmake
+rm -rf FileSystemTests
+rm -rf ImageConverterTests
+rm -rf ImageMergerTests
+rm -rf libImageToolkitCore.a
+rm -rf Makefile
