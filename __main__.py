ce4a6e68e706c986cfa64c8fdffaa3daac74a2c3
ACFPeacekeeper <afonso.fernandes100@gmail.com>
2025-11-21 21:34:10 +0000

    Made drive sync read credentials from json vault

--- __main__.py
+++ __main__.py
@@ -1,11 +1,12 @@
 import os
 import sys
 
-from backend.src import launch_app
+from backend.src import launch_app, log_uncaught_exceptions
 
 # Ensure that your root directory is on the path if needed
 sys.path.insert(0, os.path.dirname(__file__))
 
 
 if __name__ == "__main__":
+    sys.excepthook = log_uncaught_exceptions
     launch_app({'no_dropdown': False})
