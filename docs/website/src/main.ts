9cdb03e8970862cc3838c49f76fa41f4e613d4f0
ACFHarbinger <afonso.fernandes100@gmail.com>
2026-08-07 06:39:33 +0100

    docs: consolidate documentation toolchains for issue #4
    - Retain MkDocs and Sphinx
    - Remove Vue 3 site (docs/website)
    - Remove Structurizr
    - Remove TypeDoc
    - Remove their build steps from .github/workflows/docs.yml
    - Update CHANGELOG

--- docs/website/src/main.ts
+++
@@ -1,8 +0,0 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
-import "./styles/theme.css";
-import "./styles/markdown.css";
-import "highlight.js/styles/github-dark.css";
-
-createApp(App).use(router).mount("#app");
