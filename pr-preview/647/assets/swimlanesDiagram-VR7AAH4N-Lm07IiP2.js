79dc80ee683e6bb905fe607bd49c90b2af12675d
ACFHarbinger <ACFHarbinger@users.noreply.github.com>
2026-09-16 15:44:00 +0000

    deploy: 2fe635b7730373b37db133e36b00c156a4a0c9a5

---
+++ pr-preview/647/assets/swimlanesDiagram-VR7AAH4N-Lm07IiP2.js
@@ -0,0 +1,8 @@
+import"./chunk-DU6HZSFF-B7yPgLwO.js";import{n as e}from"./chunk-Y2CYZVJY-DsF7k-Jl.js";import"./src-B6xuSHsQ.js";import"./chunk-75Z2AOVW-BFRoNPUA.js";import"./chunk-P2QGCYS3-DU706KhY.js";import"./chunk-PWAF6VOD-C30zzUv5.js";import"./chunk-GMAD6QVW-DBH9Ptra.js";import"./chunk-4HAMMTFA-D1P_xFpH.js";import"./chunk-GVQU2GXP-UoWKM3vP.js";import"./index-CgwvShaj.js";import"./chunk-F27PBJKO-BcZruNv8.js";import"./chunk-XXDRQBXY-DqAYeljN.js";import"./chunk-POPQ4Y6H-BZYJnZHl.js";import{r as t,t as n}from"./chunk-SHT3W25Y-BC-ZnhgW.js";var r=n({defaultLayout:`swimlane`,styles:e(e=>`${t(e)}
+  .swimlane.cluster rect {
+    stroke: ${e.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,`getStyles`)});export{r as diagram};
